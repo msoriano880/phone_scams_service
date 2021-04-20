91ce8ef4117fae5f7abd76135a044bb04cbe067c
mc paul jr <msoriano@deakin.edu.au>
2021-04-20 22:13:47 +1000

    created fetching data feature

---
+++ client.js
@@ -0,0 +1,33 @@
+//elements
+const Phone_Number = document.querySelector('#phone_number');
+const message = document.querySelector('#message');
+
+function getData() {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ Phone_Number: Phone_Number.value }),
+    };
+    fetch('/api', options)
+      .then((res) => res.json())
+      .then(function(data) {
+        if (data.status === 'success') {
+          message.textContent = 'NOT TRUSTED!';
+          message.className = 'red-text';
+        } else {
+          message.textContent = 'TRUSTED';
+          message.className = 'green-text';
+        }
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+};
+
+document.querySelector('#btn_submit').addEventListener('click', function (error) {
+    error.preventDefault();
+    getData();
+});
+
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const path = require('path');
+const ScrapedData = require('./schema');
 
 //connection to mongodb
 mongoose.connect('mongodb+srv://msoriano:goldtree299@scrapednumbers.pyjdr.mongodb.net/scrapedNumbers?retryWrites=true&w=majority',
@@ -11,6 +12,26 @@ mongoose.connect('mongodb+srv://msoriano:goldtree299@scrapednumbers.pyjdr.mongod
 
 const publicFolder = path.join(__dirname, './public');
 app.use(express.static(publicFolder));
+
+app.use(express.json());
+app.use(express.static('./'));
+
+//get users data and finds in the mongodb
+app.post('/api', function(req, res) {
+    ScrapedData.findOne(req.body)
+        .then(function(data) {
+          res.json({
+            status: 'success',
+            Phone_Number: data.Phone_Number,
+            VictimAndCountry: data.country,
+            Details: data.Details
+        });
+    })
+    .catch(function(error) {
+        console.log('trusted number')
+        res.json(error);
+    });
+});
 
 //connection to server
 app.listen(3000, function(req, res){
